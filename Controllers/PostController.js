bfdce4108eb90a9d29e9d13d5074c3b68b1bc534
Guilherme Sella <guilhermesella1974@gmail.com>
2023-09-17 17:11:34 -0300

    Rotas e métodos para criação, exclusão e atualização de posts

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -6,7 +6,34 @@ export const getPosts = async(req,res)=>{
 }
 
 export const getUniquePost = async(req,res)=>{
-    const idPost = req.params.id
+    const idPost = req.params.idPost
     const post = await Post.findByPk(idPost)
     return res.status(200).json(post)
+}
+
+export const createPost = async(req,res)=>{
+    const post = await Post.create({
+        Usuario:req.body.Usuario,
+        Texto:req.body.Texto
+    })
+    
+}
+export const updatePost = async(req,res)=>{
+    const post = await Post.update({
+        Usuario:req.body.Usuario,
+        Texto:req.body.Texto
+    },
+    { 
+        where:{idPost:req.params.id}
+    }
+    
+    )
+}
+
+export const deletePost = async(req,res)=>{
+    await Post.destroy({
+        where:{
+            idPost:req.params.id
+        }
+    })
 }
--- Rotas/routes.js
+++ Rotas/routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {getPosts, getUniquePost} from "../Controllers/PostController.js"
+import {getPosts, getUniquePost, createPost, updatePost, deletePost} from "../Controllers/PostController.js"
 
 
 
@@ -9,3 +9,9 @@ export const router  = express.Router();
 router.get("/posts", getPosts);
 
 router.get("/post/:id", getUniquePost);
+
+router.post("/post", createPost );
+
+router.put("/post/:id", updatePost);
+
+router.delete("/post/:id", deletePost);
